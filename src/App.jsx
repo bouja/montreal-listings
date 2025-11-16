6b2dc5e7a1f1af83294263bdeb012de890de5013
bouja <33529141+bouja@users.noreply.github.com>
2025-11-16 15:01:34 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import blogPosts from './data/posts';
+const AFFILIATE = { hotels: '#', rentals: '#', localDeals: '#' };
+export default function App() { return <div>Hello Montreal Affiliate</div>; }
