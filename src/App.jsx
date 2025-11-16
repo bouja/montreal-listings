ea44cd32a62deb22d9f0e48a19090e33023c1e82
bouja <33529141+bouja@users.noreply.github.com>
2025-11-16 10:29:08 -0800

    Delete src directory

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import blogPosts from './data/posts';
-const AFFILIATE = { hotels: '#', rentals: '#', localDeals: '#' };
-export default function App() { return <div>Hello Montreal Affiliate</div>; }
--- src/data/posts.js
+++
@@ -1,4 +0,0 @@
-const blogPosts = [
-  { id: "sample-post", title: "Sample Post", description: "Description", image: "https://images.unsplash.com/photo-1", datePublished: "2025-11-11", author: "Montreal Listings", slug: "/blog/sample-post", contentSnippet: "Snippet", contentEN: "EN content", contentFR: "FR content", ctaBuy: "https://affiliate.com/buy", ctaRent: "https://affiliate.com/rent" }
-];
-export default blogPosts;
