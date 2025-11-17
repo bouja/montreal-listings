77495c35f0dee37464603cd333dc9fffa9710b51
bouja <33529141+bouja@users.noreply.github.com>
2025-11-17 12:29:27 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/globals.css';
 import dynamic from 'next/dynamic';
+
 const Analytics = dynamic(() => import('../utils/analytics'), { ssr: false });
 
 export default function App({ Component, pageProps }) {
