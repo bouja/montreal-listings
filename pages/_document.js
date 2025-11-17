17aa21aad6a65a06e28d13c2e16fe99cfb1dcfe7
bouja <33529141+bouja@users.noreply.github.com>
2025-11-17 12:28:55 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en-CA">
+    <Html lang="en-CA" dir="ltr">
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="index, follow" />
       </Head>
       <body>
         <Main />
